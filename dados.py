2f19d8c8343148a09ea3485a66348375038765a3
vinicius.oliveira <vinicius.oliveira@cwi.com.br>
2017-06-07 18:10:01 -0300

    ajuste

--- dados.py
+++ dados.py
@@ -12,7 +12,7 @@ def carregar_acessos():
 
 	for home, como_funciona, contato, comprou in leitor:
 
-	    x.append([int(acessou_home), int(acessou_como_funciona), int (acessou_contato)])
+	    x.append([int(home), int(como_funciona), int (contato)])
 	    y.append(int(comprou))
 
 	return x, y    
