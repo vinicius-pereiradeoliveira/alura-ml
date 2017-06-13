9db3ab7dafa00ba8770f1c1c515f621935f3ae9c
vinicius.oliveira <vinicius.oliveira@cwi.com.br>
2017-06-13 16:34:33 -0300

    exercicios alura ML

---
+++ classifica_busca.py
@@ -0,0 +1,3 @@
+import pandas as pd
+dados = pd.read_csv('busca.csv')
+print(dados)
--- dados.py
+++ dados.py
@@ -15,4 +15,21 @@ def carregar_acessos():
 	    x.append([int(home), int(como_funciona), int (contato)])
 	    y.append(int(comprou))
 
+	return x, y
+
+def carregar_buscas():
+
+	x = []
+	y = []
+
+	arquivo = open('busca.csv', 'rb')
+	leitor = csv.reader(arquivo)
+
+	leitor.next()
+
+	for home, busca, logado, comprou in leitor:
+
+	    x.append([int(home), busca, int (logado)])
+	    y.append(int(comprou))
+
 	return x, y    
